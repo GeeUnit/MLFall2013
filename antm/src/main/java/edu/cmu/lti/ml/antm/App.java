1ac8731bba4a3ff51fab7cd5b805010d9801a5ed
Jeff <jeffgee@cmu.edu>
2013-10-25 15:40:43 -0400

    First commit with weka-dev library dependency

---
+++ antm/src/main/java/edu/cmu/lti/ml/antm/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.lti.ml.antm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
